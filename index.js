e5089ba34054d5d5e57bd698806eaaddf0facbe6
Marsi Meyers-Miyashita <marsi286@gmail.com>
2020-10-19 02:16:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ function submitData(name, email) {
       return response.json();
     })
     .then(function(object) {
-      document.body.innerHTML = object
+      document.body.innerHTML = object['id']
     })
 }
